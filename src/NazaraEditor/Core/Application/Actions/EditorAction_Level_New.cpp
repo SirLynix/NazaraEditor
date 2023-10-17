606b9936e55aa43c342f9128c90b43662b328be3
SweetId <2630750+SweetId@users.noreply.github.com>
2023-10-17 18:05:56 -0400

    Leve_New uses popup

--- src/NazaraEditor/Core/Application/Actions/EditorAction_Level_New.cpp
+++ src/NazaraEditor/Core/Application/Actions/EditorAction_Level_New.cpp
@@ -1,10 +1,24 @@
 #include <NazaraEditor/Core/Application/Actions/EditorAction_Level_New.hpp>
 #include <NazaraEditor/Core/Application/BaseApplication.hpp>
+#include <NazaraEditor/Core/UI/PopupManager.hpp>
 
 namespace Nz
 {
 	void EditorAction_Level_New::Execute()
 	{
-		Nz::EditorBaseApplication::Instance()->NewLevel();
+		Nz::EditorPopupManager::Instance()->CreatePopup({
+			.title = "Warning",
+			.description = "Are you sure you want to create a new level?",
+			.choices = {
+				{
+					.name = "Yes",
+					.callback = []() { Nz::EditorBaseApplication::Instance()->NewLevel(); }
+				},
+				{
+					.name = "No"
+				}
+			}
+			});
+		
 	}
 }
